452b95e3b6999e5117f84c64491393e45aa43233
pranjuldhiman <pranjuldheeman@gmail.com>
2022-03-13 21:42:55 +0530

    init commit

---
+++ app/src/main/java/com/macamp/noteappjetpack/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.macamp.noteappjetpack
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.macamp.noteappjetpack.ui.theme.NoteAppJetpackTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NoteAppJetpackTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Pranjul")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    NoteAppJetpackTheme {
+        Greeting("Android")
+    }
+}
